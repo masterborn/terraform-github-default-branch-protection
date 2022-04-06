e93f75052178792acb7c3d37fdedd6c6cd8538cd
Damian Kokot <damian.k@masterborn.com>
2022-04-06 16:06:46 +0200

    docs: extend example

---
+++ examples/basic/main.tf
@@ -0,0 +1,8 @@
+module "some-project-branch-protection" {
+  source = "git@github.com:masterborn/terraform-github-default-branch-protection.git?ref={{ provider_version }}"
+
+  repository_name = module.some-project.repository_name
+  use_develop     = true
+  use_stage       = true
+  use_master      = true
+}
